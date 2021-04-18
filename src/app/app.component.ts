c8f82054687b49033f91d3c11e76cf9b250782a7
Harish Ingole <harish.ingole@vinsys.com>
2021-04-18 10:20:43 +0530

    "ngForm"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild} from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('f') signupForm : NgForm;
+  defaultQuestion ='pet';
+  answere="";
+  genders=['Male','Female'];
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData:{
+    //     username:'johnmalik',
+    //     email:'johnmalik@gmail.com'
+    //   },
+    //   secret:'pet',
+    //   quaestionAnswere:'dfds bvb vgrgb',
+    //   gender:'Male'
+    // })
+    this.signupForm.form.patchValue({
+      userData:{
+        username:'johnmalik',
+      }
+    })
+  }
+  onSubmit(){
+    console.log(this.signupForm);
   }
 }
